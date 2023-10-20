42078833635e8b0d5415e1e448ab7a62e9935674
elaheerazob <ridwanelahee282@gmail.com>
2023-10-21 00:16:12 +0600

    PUT api

--- src/app/api/hello/[id]/route.js
+++ src/app/api/hello/[id]/route.js
@@ -8,3 +8,16 @@ export function GET(request,content){
     const userData = user.filter((item)=>item.id==content.params.id)
     return NextResponse.json(userData.length==0?{result:"No result found",success:false}:{result:userData[0],success:true},{status:200})
 }
+
+export async function PUT(request,content) {
+    let payload = await request.json();
+    payload.id= content.params.id;
+    console.log(payload);
+
+  if(!payload.id || !payload.name || !payload.age || !payload.email)
+  {
+    return NextResponse.json({result:"no data",success:false},{status:400})
+  }
+
+    return NextResponse.json({result:payload,success:true},{status:200})
+}
