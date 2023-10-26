fc48d13bae6a6b2f52e79e640957aec3e15665c7
elaheerazob <ridwanelahee282@gmail.com>
2023-10-27 00:27:13 +0600

    update data

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import './../style.css';
 
 async function getUsers(){
     let data = await fetch('http://localhost:3000/api/hello');
@@ -15,9 +16,10 @@ export default async function page(){
         <h1>This is users page</h1>
         {
             user.map((i)=>(
-                <div>
+                <div className="user-item">
                     {i.id}
-                    <Link href={`/users/${i.id}`}>{i.name}</Link>
+                    <span><Link href={`/users/${i.id}`}>{i.name}</Link></span>
+                    <span><Link href={`/users/${i.id}/update`}>Edit</Link></span>
                 </div>
             ))
         }
