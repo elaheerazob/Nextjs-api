ac23cc5c2e114622b320b95d5dc722ab98b782c7
elaheerazob <ridwanelahee282@gmail.com>
2023-10-28 23:46:14 +0600

    put data update

--- src/app/users/[userid]/update/page.js
+++ src/app/users/[userid]/update/page.js
@@ -1,41 +1,70 @@
-"use client"
-import './../../../style.css'
+"use client";
+import "./../../../style.css";
 
 import { useEffect, useState } from "react";
 
-export default function page({params}){
-    let id =(params.userid);
-    const [name,setName]=useState("");
-    const [age,setAge]=useState("");
-    const [email,setEmail]=useState("");
+export default function page({ params }) {
+  let id = params.userid;
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
 
-    useEffect (()=>{
-        getUserDetails()
-    },[])
+  useEffect(() => {
+    getUserDetails();
+  }, []);
 
-    const getUserDetails= async()=>{
-        let data= await fetch('http://localhost:3000/api/hello/'+id);
-        data =await data.json()
-        setName(data.result.name)
-        setAge(data.result.age)
-        setEmail(data.result.email)
+  const getUserDetails = async () => {
+    let data = await fetch("http://localhost:3000/api/hello/" + id);
+    data = await data.json();
+    setName(data.result.name);
+    setAge(data.result.age);
+    setEmail(data.result.email);
+  };
+
+  const updateData = async () => {
+    let result = await fetch("http://localhost:3000/api/hello/" + id, {
+      method: "PUT",
+      body: JSON.stringify({ name, age, email }),
+    });
+    result = await result.json()
+    console.log(result);
+    if(result.success){
+        alert("Data Update success")
+    }else{
+        alert("Please Insert data Again")
     }
+  };
 
-    const updateData =()=>{
-        console.log(name,age,email);
-    }
+  console.log(id);
+  return (
+    <div>
+      <h1>Update Details</h1>
 
+      <input
+        className="input-filed"
+        type="text"
+        value={name}
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        className="input-filed"
+        type="text"
+        value={age}
+        placeholder="Age"
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <input
+        className="input-filed"
+        type="text"
+        value={email}
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
 
-    console.log(id);
-    return (
-        <div>
-            <h1>Update Details</h1>
-
-            <input className="input-filed" type="text" value={name} placeholder="Name" onChange={(e)=>setName(e.target.value)}/>
-            <input className="input-filed" type="text" value={age} placeholder="Age" onChange={(e)=>setAge(e.target.value)} />
-            <input className="input-filed" type="text" value={email} placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
-
-            <button onClick={updateData} className='btn'>Update Data</button>
-        </div>
-    )
+      <button onClick={updateData} className="btn">
+        Update Data
+      </button>
+    </div>
+  );
 }
