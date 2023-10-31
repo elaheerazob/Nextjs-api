750e9657b23dbd809a08e0fe46d24990cce2855c
elaheerazob <ridwanelahee282@gmail.com>
2023-11-01 00:11:32 +0600

    delate data

--- src/app/api/hello/[id]/route.js
+++ src/app/api/hello/[id]/route.js
@@ -21,3 +21,12 @@ export async function PUT(request,content) {
 
     return NextResponse.json({result:payload,success:true},{status:200})
 }
+
+export function DELETE(request,content){
+  let id =content.params.id;
+  if(id){
+    return NextResponse.json({result:"User Deleted",success:true},{status:200})
+  }else{
+    return NextResponse.json({result:"Something is worng try again",success:false},{status:400})
+  }
+}
